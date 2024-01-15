1f3adab58c2d67933f674cb043052359623836ca
uBlock-user <21290713+uBlock-user@users.noreply.github.com>
2024-01-15 20:55:16 +0530

    Delete tools directory

--- tools/make-anticircumventionfilterlist.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# This script assumes a linux environment
-
-echo "*** Anti-Circumvention-Filterlist: Assembling acf.txt"
-node ./tools/make-anti-circumvention-filterlist.mjs in=tools/anti-circumvention-filterlist.template out=acf.out.txt
